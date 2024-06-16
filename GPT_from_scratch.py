62e9e313dc03de4bcd7475f7dcf125bbd564b33f
Antonio Emmanuele Renda <emmanuele.renda@gmail.com>
2024-06-16 22:03:26 +0200

    Initial imports.

---
+++ GPT_from_scratch.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import time
+import math
+import argparse
+from dataclasses import dataclass
+from typing import List
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+words = open ('name.txt', 'r').read().splitlines()
+
